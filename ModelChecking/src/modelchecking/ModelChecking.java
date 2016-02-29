d42b2e305a4fc9501a58d23583177dc0ff7e98c5
Ruud Andriessen <r.andriessen@ilysian.com>
2016-02-29 19:04:19 +0100

    Implemented uParser for tree based uCalculus representation

--- ModelChecking/src/modelchecking/ModelChecking.java
+++ ModelChecking/src/modelchecking/ModelChecking.java
@@ -5,6 +5,12 @@
  */
 package modelchecking;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ruudandriessen
@@ -15,10 +21,24 @@ public class ModelChecking {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        AldebaranParser a = new AldebaranParser();
-        //System.out.println(a.readLTS().toString());
-        System.out.println(a.readLTS().toDot());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true) {
+            System.out.println("----------------------");
+            System.out.println("Model checking v0.1a");
+            System.out.println("Please enter uInput: ");
+            String s = "";
+            try {
+            String s = br.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(ModelChecking.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            s = s.replaceAll("\\s+","");
+            s = s.replaceAll("nu","nu ");
+            s = s.replaceAll("mu","mu ");
+            uFunction f = new uFunction(s);
+            System.out.println(f);
+        }
     }
     
 }
