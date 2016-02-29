85476b7e7eef9cedc2dbd6eca897aae05e74bff8
HeinvanBeers <heinvanbeers@hotmail.com>
2016-02-29 15:48:52 +0100

    Aldebaran Parser

--- ModelChecking/src/modelchecking/ModelChecking.java
+++ ModelChecking/src/modelchecking/ModelChecking.java
@@ -16,6 +16,8 @@ public class ModelChecking {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        AldebaranParser a = new AldebaranParser();
+        a.readLTS();
     }
     
 }
